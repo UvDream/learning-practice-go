02b7424fca4ad2f9e5ad727bc53bb6027bdd12b1
wangzhongjie <UvDream@163.com>
2019-11-27 13:44:33 +0800

    feat:tada: 变量声明

--- 40-day/day01/main.go
+++ 40-day/day01/main.go
@@ -3,6 +3,17 @@ package main
 import (
 	"fmt"
 )
+var (
+	name string
+	age int
+	isOk bool
+)
+
 func main(){
-	fmt.Println("hellow")
+	name="张三"
+	age=18
+	isOk=true
+	fmt.Print(isOk,"\n")
+	fmt.Printf("name:%s\n",name)	//%s:占位符,使用name这个变量的值去替换占位符
+	fmt.Println(age)			//打印完指定的内容之后会在后面加上一个换行符
 }
